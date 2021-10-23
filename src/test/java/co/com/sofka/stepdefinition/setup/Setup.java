393a3e30a72c1ea6099a46c5633356e8a563fcd5
ajcoque <84734358+ajcoque@users.noreply.github.com>
2021-10-23 18:29:00 -0500

    Se agregaron configuraciones para el Log4j2

--- src/test/java/co/com/sofka/stepdefinition/setup/Setup.java
+++ src/test/java/co/com/sofka/stepdefinition/setup/Setup.java
@@ -3,34 +3,42 @@ package co.com.sofka.stepdefinition.setup;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.serenitybdd.screenplay.rest.questions.LastResponse;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+
+import static co.com.sofka.util.Log4jValues.LOG4J_PROPERTIES_FILE_PATH;
 
 public class Setup {
     protected static final String URL_BASE = "https://jsonplaceholder.typicode.com";
     protected static final String RESOURCE_POST = "/posts/1";
     protected static final String RESOURCE_POSTCOMMENT = "/comments?postId=1";
+    protected static final String USER_DIR = System.getProperty("user.dir");
 
     protected Actor actor;
 
-    private void setUpActor(){
+    private void setUpActor() {
         actor = Actor.named("Usuario1");
     }
 
-    private void setUpActorAndApi(){
+    private void setUpActorAndApi() {
         actor.can(CallAnApi.at(URL_BASE));
     }
 
-    protected void setUp(){
+    protected void setUp() {
+        setUpLog4j2();
         setUpActor();
         setUpActorAndApi();
     }
 
-    protected String fromLastResponseBy(Actor actor){
-        return  new String(
+    protected String fromLastResponseBy(Actor actor) {
+        return new String(
                 LastResponse.received().answeredBy(actor).asByteArray(),
                 StandardCharsets.UTF_8
         );
+    }
+
+    protected void setUpLog4j2() {
+        PropertyConfigurator.configure(USER_DIR + LOG4J_PROPERTIES_FILE_PATH.getValue());
     }
 }
